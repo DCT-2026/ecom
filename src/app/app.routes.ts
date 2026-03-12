391e72391d606d7da0c9868d34aed9f609db9baf
Vignesh M <vi2003gnesh@gmail.com>
2026-03-12 19:33:46 +0530

    create car mobile accessories pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-
+import { ProductList } from './pages/product-list/product-list';
+import {MobileProduct} from './pages/mobile-product/mobile-product'
 export const routes: Routes = [
     {
         path:'', component:Home
+    },{
+        path:'products',component:ProductList
+    },{
+        path:'Mobile',component:MobileProduct
     }
 ];
