2afc31bc18281dca4fea6f6edbfef14064b070d7
Lucifer <lucifer@example.com>
2026-03-12 19:45:54 +0530

    create blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Blog } from './blog/blog';
 
 export const routes: Routes = [
     {
-        path:'', component:Home
+        path:'', component:Blog
     }
 ];
