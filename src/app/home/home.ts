f2f6386005576fcda0af9e31ec2a04aee7158c86
Infantpranav <pranavselvam2006@gmail.com>
2026-03-15 18:24:59 +0530

    product

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { Header } from "../header/header";
-import { Footer } from "../footer/footer";
-import { HeroCarousel } from "../hero-carousel/hero-carousel";
+import { Product } from "../product/product";
+
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer, HeroCarousel],
+  imports: [ Product],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
