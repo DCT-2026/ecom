154f70a50e297ff6c9fca4d2831829f0b062ee00
SubburamK <subburam@e2o.in>
2026-03-15 18:35:02 +0530

    merge all codes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Product } from "../product/product";
 import { Categories } from '../categories/categories';
 import { CommonModule } from '@angular/common';
-import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { HeroCarousel } from '../hero-carousel/hero-carousel';
 import {CarAccessoriesComponent} from '../car-accessories/car-accessories'
@@ -15,7 +14,6 @@ import {MobileAccessories} from '../mobile-accessories/mobile-accessories'
   imports: [
     CommonModule,
     Categories,
-    Header,
     Footer,
     Product,
     HeroCarousel,CarAccessoriesComponent,MobileAccessories
